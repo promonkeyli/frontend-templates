d30448ea46c5520bdb251c4169e60d0248f73e0a
Young <liyang_email@163.com>
2024-06-03 11:27:09 +0800

    feat(next-web-tpl): upgrade Next.js 15 RC

--- next-web-tpl/src/app/layout.tsx
+++ next-web-tpl/src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
-import type React from "react";
+// biome-ignore lint/style/useImportType: <explanation>
+import React from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = localFont({
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+});
+const geistMono = localFont({
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -17,7 +25,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+				{children}
+			</body>
 		</html>
 	);
 }
